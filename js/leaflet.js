9d1fdeaf04eddf2a37b42afd0956cb5e7d30829d
Jędrzej Sypka <jj@macbook-air-jedrzej.home>
2024-04-18 17:07:06 +0200

    projekt

--- js/leaflet.js
+++
@@ -1,9 +0,0 @@
-var mapOptions = {
-    center: [50.0689,19.9547],
-    zoom: 10
- }
- var map = new L.map('map', mapOptions);
-
- var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
- 
- map.addLayer(layer);
